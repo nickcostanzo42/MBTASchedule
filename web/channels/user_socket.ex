229ef8771d7d08964de761d2b6f55e89ddceac73
Nick Costanzo <nickcostanzo42@gmail.com>
2017-01-26 18:30:07 -0500

    set up secret keys and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,6 +6,8 @@ defmodule MbtaSchedule.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
+    timeout: 45_000
+  end
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
