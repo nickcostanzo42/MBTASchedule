4f28f0153a38242d371f670185b060bfc004d4b0
Nick Costanzo <nickcostanzo42@gmail.com>
2017-01-26 18:50:38 -0500

    update

--- lib/mbta_schedule/repo.ex
+++ lib/mbta_schedule/repo.ex
@@ -1,3 +1,11 @@
 defmodule MbtaSchedule.Repo do
   use Ecto.Repo, otp_app: :mbta_schedule
+
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "my_app_dev",
+  pool_size: 10
+
 end
+
